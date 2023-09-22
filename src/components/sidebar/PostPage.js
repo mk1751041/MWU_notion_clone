ac2d7d60213434a1a12b653dc78e646e2a47d029
mk1751041 <losa05231998@gmail.com>
2023-09-22 21:11:07 +0900

    New Button 만들기

--- src/components/sidebar/PostPage.js
+++ src/components/sidebar/PostPage.js
@@ -12,4 +12,10 @@ export default function PostPage({ $target, initialState }){
         $target: $page, 
         data: initialState
     })
+
+    // TODO: 컴포넌트화 시키기
+    const $newBtn = document.createElement('button')
+    $newBtn.textContent = '+ New Page'
+    $newBtn.className = 'addNew'
+    $page.appendChild($newBtn)
 }
